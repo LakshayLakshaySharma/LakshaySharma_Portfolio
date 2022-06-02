fb97185b79cb0d9e326d0c261824972bc8d78b63
Lakshay <lakshay0sharma@gamil.com>
2022-06-02 18:52:17 +0530

    modified breifintro component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import BriefIntro from './components/BriefIntro';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
   <div>
    <Navbar/>
+   <BriefIntro/>
   </div>
   );
 }
---
+++ src/components/BriefIntro.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function BriefIntro() {
+  return (
+    <div>
+        <div>
+            <h1>HI</h1>
+            <h2>I AM LAKSHAY SHARMA</h2>
+            <h4>FRONT END WEB DEVELOPER</h4>
+            <img src="portfolio/lak.png" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default BriefIntro
