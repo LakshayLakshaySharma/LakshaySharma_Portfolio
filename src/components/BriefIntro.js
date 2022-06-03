0cbf9e69e8b339e30235c7660e393dbe38f1c3ca
Lakshay <lakshay0sharma@gamil.com>
2022-06-03 13:33:39 +0530

    creating HOME page

--- src/components/BriefIntro.js
+++ src/components/BriefIntro.js
@@ -1,13 +1,16 @@
 import React from 'react'
+// import img from './image/lak.png'
 
 function BriefIntro() {
+
   return (
+
     <div>
         <div>
             <h1>HI</h1>
             <h2>I AM LAKSHAY SHARMA</h2>
             <h4>FRONT END WEB DEVELOPER</h4>
-            <img src="portfolio/lak.png" alt="" />
+            <img src='./image/lak.png' alt="" style={{height : '200px', width :"200px", borderRadius : '50%'}}/>
         </div>
     </div>
   )
