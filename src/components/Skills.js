7c6ca0474c1ea0fc904220f6b62cc854d9ec1d35
Lakshay <lakshay0sharma@gamil.com>
2022-06-05 13:13:30 +0530

    modified skill components

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,24 +1,47 @@
-import React from 'react'
+import React from "react";
 
 function Skills() {
   return (
-    <div style={{height : '100vh', border : '1px solid black'}}>
+    <div style={{ height: "100vh", border: "1px solid black" }}>
       <div>
-        <div className ="container">
+        <div className="container">
           <h2>The Skills i learn</h2>
-           <div>
-           <i class="fa-brands fa-html5"></i>
-           <i class="fa-brands fa-css3-alt"></i>
-           <i class="fa-brands fa-js-square"></i>
-           <i class="fa-brands fa-react"></i>
-           <i class="fa-brands fa-git"></i>
-           <i class="fa-brands fa-github"></i>
-           <i class="fa-brands fa-bootstrap"></i>
-           </div>
+          <div  style={{ border: "1px solid red", }}>
+
+            <div className="d-flex justify-content-center">
+              <p style={{ fontSize: "100px", color : '#e34c26' , paddingRight : '50px'}}>
+                <i className="fa-brands fa-html5"></i>
+              </p>
+              <p style={{ fontSize: "100px", color :'#3C99DC', paddingRight : '50px'}}>
+                <i className="fa-brands fa-css3-alt"></i>
+              </p>
+              <p style={{ fontSize: "100px", color : '#F0DB4F', paddingRight : '50px'}}>
+                <i className="fa-brands fa-js-square"></i>
+              </p>
+            </div>
+
+            <div className="d-flex justify-content-center">
+              <p style={{ fontSize: "100px", color : '#3C99DC', paddingRight : '50px'}}>
+                <i className="fa-brands fa-react"></i>
+              </p>
+              <p style={{ fontSize: "100px" , color : '#3E2c00', paddingRight : '50px'}}>
+                <i className="fa-brands fa-git"></i>
+              </p>
+              <p style={{ fontSize: "100px", color : '#6e5494' ,paddingRight : '50px'}}>
+                <i className="fa-brands fa-github"></i>
+              </p>
+            </div>
+
+            <div className="d-flex justify-content-center">
+              <p style={{ fontSize: "100px", color :'#080135' }}>
+                <i className="fa-brands fa-bootstrap"></i>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Skills
+export default Skills;
