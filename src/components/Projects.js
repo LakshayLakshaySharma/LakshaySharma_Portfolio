6db7e7d668838b595704c882ed2458fcc345a615
Lakshay <lakshay0sharma@gamil.com>
2022-06-06 22:01:09 +0530

    add more sample project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,8 +4,9 @@ function Projects() {
   return (
     <div className="container">
       <div className="container row">
-        <h1>Work Sample</h1>
+        <h1>React Sample </h1>
         {/* react info project */}
+        
         <div className="col-md-4">
           <div class="card" style={{ width: "18rem" }}>
             <img
@@ -112,9 +113,332 @@ function Projects() {
         </div>
       </div>
 
-{/* more work sample */}
+{/* more work sample, javascript project */}
       <div>
+        <h1>Javascript Work Sample</h1>
+      <div>
+      <div className="row">
+          <div className="col-md-4">
+            {/* tip calcualtor */}
+            <div class="card" style={{ width: "18rem" }}>
+              <img
+                src="https://i.pinimg.com/564x/cc/d7/be/ccd7be64792bd1238be0d5efad47631b.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <h5 class="card-title">Tip Calculator</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a
+                  href="https://cdpn.io/pen/debug/NWgqbaY?authentication_hash=VGrWNooQgYjM"
+                  target="_blank"
+                  rel="noreferrer"
+                  class="btn btn-primary"
+                >
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
 
+          <div className="col-md-4">
+            {/* calcualtor */}
+            <div class="card" style={{ width: "18rem" }}>
+              <img
+                src="https://i.pinimg.com/736x/6e/75/90/6e7590fd49ac3c2198961a128d128b20.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <h5 class="card-title"> Calculator</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a
+                  href="https://cdpn.io/pen/debug/wvdREjg?authentication_hash=VGAWNooQaGdr"
+                  target="_blank"
+                  rel="noreferrer"
+                  class="btn btn-primary"
+                >
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            {/* hex color changer */}
+            <div class="card" style={{ width: "18rem" }}>
+
+              <img
+                src="https://i.pinimg.com/564x/b1/0e/fb/b10efbb46769f49271592e3b682e4948.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <h5 class="card-title">Hex Color Changer</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a
+                  href="https://cdpn.io/pen/debug/abWMPrj?authentication_hash=LQMExNNVdWzk"
+                  target="_blank"
+                  rel="noreferrer"
+                  class="btn btn-primary"
+                >
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            {/* todo list */}
+            <div class="card" style={{ width: "18rem" }}>
+              <img
+                src="https://i.pinimg.com/564x/81/cb/be/81cbbee1fecc4ab241d7b3c8a3270aaa.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <h5 class="card-title">To-Do List</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a
+                  href="https://cdpn.io/pen/debug/yLbvbaq?authentication_hash=vWkRwooWDxKM"
+                  target="_blank"
+                  rel="noreferrer"
+                  class="btn btn-primary"
+                >
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            {/* css change */}
+            <div class="card" style={{ width: "18rem" }}>
+              <img
+                src="https://i.pinimg.com/564x/8c/c4/a9/8cc4a9a172f7bd1cf033a3644ce3c044.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <h5 class="card-title">Change CSS</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a
+                  href="https://cdpn.io/pen/debug/abWpoOP?authentication_hash=RBMOJbbyBwzk"
+                  target="_blank"
+                  rel="noreferrer"
+                  class="btn btn-primary"
+                >
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            {/* guess random number */}
+            <div class="card" style={{ width: "18rem" }}>
+              <img
+                src="https://i.pinimg.com/564x/7d/1d/b2/7d1db240475649bc635c085dc11fd6f3.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <h5 class="card-title">Guess Random Number</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a
+                  href="https://cdpn.io/pen/debug/WNjyExm?authentication_hash=ZoABaQQoGBVr"
+                  target="_blank"
+                  rel="noreferrer"
+                  class="btn btn-primary"
+                >
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+
+{/* html css project */}
+      <div>
+        <div className='row'>
+          <h1>HTML & CSS Project</h1>
+        <div className="col-md-4">
+            {/* Tribute Page */}
+            <div class="card" style={{ width: "18rem" }}>
+              <img
+                src="https://i.pinimg.com/564x/94/b4/54/94b454f14c145475b78da427122b29bb.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <h5 class="card-title">Tribute Page</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a
+                  href="https://cdpn.io/pen/debug/ZEWdVzR?authentication_hash=GnMnbqqnywNM"
+                  target="_blank"
+                  rel="noreferrer"
+                  class="btn btn-primary"
+                >
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            {/* Parallax effect */}
+            <div class="card" style={{ width: "18rem" }}>
+              <img
+                src="https://i.pinimg.com/564x/d8/13/db/d813dbbcfeb97cac41f5be92e7c50cfb.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <h5 class="card-title">Parallax Effect</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a
+                  href="https://cdpn.io/pen/debug/BazBojb?authentication_hash=RBAOJbbyaxar"
+                  target="_blank"
+                  rel="noreferrer"
+                  class="btn btn-primary"
+                >
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            {/* Document page */}
+            <div class="card" style={{ width: "18rem" }}>
+              <img
+                src="https://i.pinimg.com/564x/0e/3c/b2/0e3cb27c26c5e468a0114279607f7f67.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <h5 class="card-title">Document Page</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a
+                  href="https://cdpn.io/pen/debug/BazKzyj?authentication_hash=VJMxxRRwQaLM"
+                  target="_blank"
+                  rel="noreferrer"
+                  class="btn btn-primary"
+                >
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            {/* Product Landing page */}
+            <div class="card" style={{ width: "18rem" }}>
+              <img
+                src="https://i.pinimg.com/564x/8b/7a/3e/8b7a3e7232ab48c0dfba88d26d8bd214.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <h5 class="card-title">Product Landing Page</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a
+                  href="https://cdpn.io/pen/debug/xxOxmXJ?authentication_hash=NjMYzppyReOr"
+                  target="_blank"
+                  rel="noreferrer"
+                  class="btn btn-primary"
+                >
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            {/*Survay from  */}
+            <div class="card" style={{ width: "18rem" }}>
+              <img
+                src="https://i.pinimg.com/564x/58/20/fc/5820fcce2129a4cf61998d520a1d2fda.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <h5 class="card-title">Survay From</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a
+                  href="https://cdpn.io/pen/debug/QWEyOOa?authentication_hash=GnAnbqqnxNZA"
+                  target="_blank"
+                  rel="noreferrer"
+                  class="btn btn-primary"
+                >
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            {/* guess random number */}
+            <div class="card" style={{ width: "18rem" }}>
+              <img
+                src="https://i.pinimg.com/564x/7d/1d/b2/7d1db240475649bc635c085dc11fd6f3.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <h5 class="card-title">Guess Random Number</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a
+                  href="https://cdpn.io/pen/debug/WNjyExm?authentication_hash=ZoABaQQoGBVr"
+                  target="_blank"
+                  rel="noreferrer"
+                  class="btn btn-primary"
+                >
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          
+        </div>
       </div>
     </div>
   );
