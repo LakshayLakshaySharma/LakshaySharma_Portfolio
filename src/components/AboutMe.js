b0fb0b8a650b095f91e3178178c32d7016ff1a01
Lakshay <lakshay0sharma@gamil.com>
2022-06-09 18:33:23 +0530

    style some files

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,24 +5,37 @@ function AboutMe() {
     <div style={{ height: "100vh", border: "1px solid black" }}>
       <div className="container">
         <div>
-        <h1>About Me</h1>
-        <div className="d-flex" style={{ fontSize : '20px', textAlign:'justify' }}>
-        <p style={{ width: "45vw", }} >
-          i have a huge interest in development and technology spectrum, my name
-          is lakshay sharma 20 and i am a self thought front end wen developer,
-          its been since 2 years that i learn front end web developemnet and the
-          skills i aquired in last 2 years are HTML,CSS,JAVSCRIPT,react js,
-          bootstrap, git, and git hub,and the leraning process goes on in
-          future. i am a well organised, smart working man with the problem
-          solving skill. excited to work and gain new experience that help me
-          learn and grow as a developer.
-        </p>
-        <img
-          src="./image/lak.png"
-          style={{ height: "270px", width: "270px", borderRadius: "50%", marginLeft:'30px', marginTop:'30px', boxShadow:'5px 5px 5px gray'}}
-          alt=""
-        />
-        </div>
+          <h1>About Me</h1>
+          <div
+            className="d-flex"
+            style={{ fontSize: "20px", textAlign: "justify" }}
+          >
+            <p style={{ width: "45vw" }}>
+              i have a huge interest in development and technology spectrum, my
+              name is lakshay sharma 20 and i am a self thought front end wen
+              developer, its been since 2 years that i learn front end web
+              developemnet and the skills i aquired in last 2 years are
+              HTML,CSS,JAVSCRIPT,react js, bootstrap, git, and git hub,and the
+              leraning process goes on in future. i am a well organised, smart
+              working man with the problem solving skill. excited to work and
+              gain new experience that help me learn and grow as a developer.
+              <br />
+              And apart from coding i have some more interest like i read books,
+              watch classical movies, shows, and anime
+            </p>
+            <img
+              src="./image/lak.png"
+              style={{
+                height: "270px",
+                width: "270px",
+                borderRadius: "50%",
+                marginLeft: "30px",
+                marginTop: "30px",
+                boxShadow: "5px 5px 5px gray",
+              }}
+              alt=""
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,32 +8,33 @@ function Skills() {
           <h2>The Skills i learn</h2>
           <div  style={{ border: "1px solid red", }}>
 
-            <div className="d-flex justify-content-center">
-              <p style={{ fontSize: "110px", color : '#e34c26' , paddingRight : '50px'}}>
-                <i className="fa-brands fa-html5"></i>
+            <div className="d-flex justify-content-center bg-image hover-zoom">
+              <p style={{ fontSize: "110px", color : '#e34c26' , paddingRight : '50px', }}>
+                <i className="fa-brands fa-html5  " ></i>
               </p>
+
               <p style={{ fontSize: "110px", color :'#3C99DC', paddingRight : '50px'}}>
-                <i className="fa-brands fa-css3-alt"></i>
+                <i className="fa-brands fa-css3-alt "></i>
               </p>
               <p style={{ fontSize: "110px", color : '#F0DB4F', paddingRight : '50px'}}>
-                <i className="fa-brands fa-js-square"></i>
+                <i className="fa-brands fa-js-square "></i>
               </p>
             </div>
 
             <div className="d-flex justify-content-center">
               <p style={{ fontSize: "110px", color : '#3C99DC', paddingRight : '50px'}}>
-                <i className="fa-brands fa-react"></i>
+                <i className="fa-brands fa-react "></i>
               </p>
               <p style={{ fontSize: "110px" , color : '#3E2c00', paddingRight : '50px'}}>
-                <i className="fa-brands fa-git"></i>
+                <i className="fa-brands fa-git "></i>
               </p>
               <p style={{ fontSize: "110px", color : '#6e5494' ,paddingRight : '50px'}}>
-                <i className="fa-brands fa-github"></i>
+                <i className="fa-brands fa-github "></i>
               </p>
             </div>
 
-            <div className="d-flex justify-content-center">
-              <p style={{ fontSize: "110px", color :'#080135' }}>
+            <div className="d-flex justify-content-center" >
+              <p style={{ fontSize: "110px", color :'#080135',}}>
                 <i className="fa-brands fa-bootstrap"></i>
               </p>
             </div>
