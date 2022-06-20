b61c2080226846d76e17e4f0c2f1d9686f1338da
Lakshay <lakshay0sharma@gamil.com>
2022-06-20 17:41:54 +0530

    correction in AboutMe.js file

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -11,12 +11,12 @@ function AboutMe() {
             style={{ fontSize: "20px", textAlign: "justify" }}
           >
             <p style={{ width: "45vw" }}>
-              i have a huge interest in development and technology spectrum, my
-              name is lakshay sharma 20 and i am a self thought front end wen
-              developer, its been since 2 years that i learn front end web
-              developemnet and the skills i aquired in last 2 years are
-              HTML,CSS,JAVSCRIPT,react js, bootstrap, git, and git hub,and the
-              leraning process goes on in future. i am a well organised, smart
+              my name is lakshay sharma 20 and i am a self thought front-end web 
+              developer, i have a huge interest in development and whole technology
+              spectrum, its been since 2 years that i learn front end web
+              development and the skills i aquired in last 2 years are
+              html, css, javascript,react js, bootstrap, git, and git hub,and the
+              learning process goes on in future. i am a well organised, smart
               working man with the problem solving skill. excited to work and
               gain new experience that help me learn and grow as a developer.
               <br />
