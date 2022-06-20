c4320ec8eb74801ef5fbaf8f35ff188b73a18c0a
Lakshay <lakshay0sharma@gamil.com>
2022-06-20 17:06:49 +0530

    try new stuffs with framer motion

--- src/components/BriefIntro.js
+++ src/components/BriefIntro.js
@@ -1,6 +1,25 @@
 import React from "react";
 import { motion } from "framer-motion";
 
+const containerVariant = {
+  init: {
+    y: 100,
+    opacity: 0,
+  },
+  anime: {
+    scale: 1.5,
+    // color: "#885EAD",
+    y: 200,
+    opacity: 1,
+    x: 100,
+    transition: {
+      duration: 1,
+      type: "tween",
+      delay: "0.2",
+    
+    },
+  },
+};
 function BriefIntro() {
   let styling = {
     // border: "1px solid black",
@@ -11,22 +30,17 @@ function BriefIntro() {
 
   return (
     <div style={styling}>
-
       <motion.div
-      style={{ width:'999px', }}
-        initial={{ y: -300, opacity: 0 }}
-        animate={{
-          scale: 1.5,
-          color: "#885EAD",
-          y: 200,
-          opacity: 1,
-          x : 100,
-          transition: { duration: 1.2 , type: 'tween', delay: '0.2'},
-        }}
+        style={{ width: "999px" }}
+        variants={containerVariant}
+        initial="init"
+        animate="anime"
+
+        // transition={{ duration: 1, type: 'tween', delay: '0.2' }}
 
         // style={{ marginTop: "25vh" }}
       >
-        <h1 style={{ fontSize: "50px", }}>HI</h1>
+        <h1 style={{ fontSize: "50px" }}>HI</h1>
         <h2 style={{ fontSize: "30px" }}>I AM LAKSHAY SHARMA</h2>
         <h4 style={{ fontSize: "20px" }}>FRONT END WEB DEVELOPER</h4>
       </motion.div>
